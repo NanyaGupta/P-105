37c2b0ad53886d5a4c051ceb4f50587b3ff57f94
NanyaGupta <73479405+NanyaGupta@users.noreply.github.com>
2021-04-05 18:57:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:"png",
+    png_quality:90
+});
+
+var camera= document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML='<img id="capturedImage" src="'+data_uri+'"/>'
+    });
+}
+
+console.log("ml5 version - ", ml5.version);
+
+var classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/DGTNrrZzg/model.json",modelLoaded)
+function modelLoaded(){
+    console.log("Model Loaded !");
+}
+
+function check(){
+Img=document.getElementById("capturedImage");
+classifier.classify(Img,gotResult);
+}
+//To classify captured image,compare with model and show the result//
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("resultObjectName").innerHTML=results[0].label;
+        document.getElementById("resultObjectAccuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
